d51daa630415a3087a92579a59bbe9f84a426af2
Ron Studený <ronstudeny@gmail.com>
2022-11-27 22:40:22 +0100

    both working

--- Program.cs
+++ Program.cs
@@ -7,14 +7,15 @@
         {
             string c = "";
             Console.Write("Ciphered: ");
-            Console.WriteLine(c = CaesarsCipher.Encipher("Hello world KYS", 15)); // j g nn q
-            Console.WriteLine("Deciphered: " + CaesarsCipher.Decipher(c, 15));
+
+            Console.WriteLine(c = CaesarsCipher.Encipher("idk why tf this doesnt workkkkk aaaaaaaaaa", 80)); 
+            Console.WriteLine("Deciphered: " + CaesarsCipher.Decipher(c, 80));
 
             Console.ReadLine();
 
-            //string v = "";
-            //Console.WriteLine(v = VignereCipher.Encipher("Hello World", "key"));
-            //Console.WriteLine(VignereCipher.Decipher(v, "key"));
+            string v = "";
+            Console.WriteLine(v = VignereCipher.Encipher("it workin?", "key"));
+            Console.WriteLine(VignereCipher.Decipher(v, "key"));
         }
     }
 
@@ -43,63 +44,59 @@
         }
 
 
-        private static char Cipher(char ch, int key, bool decipher = false)
+        private static char Cipher(char ch, int key, bool reverse = false)
         {
             if (!char.IsLetter(ch)) return ch;
 
-            if (!decipher)
-            {
-                if (ch + key > 122) return (char)((ch + key) - 26);
-                else return (char)(ch + key);
-            }
-            else
-            {
-                if (ch - key < 97) return (char)((ch - key) + 26);
-                else return (char)(ch - key);
-            }
 
-        }    
+            key = key % 26;
+            if (!reverse) return (char)(((ch + key - 97) % 26) + 97);
+            else return (char)(((ch + 26 - key - 97) % 26) + 97 );
 
-
-        public class VignereCipher
-        {
-            public static string Encipher(string input, string key)
-            {
-                string res = "";
-                int keyIndex = 0;
-
-                for (int i = 0; i < input.Length; i++)
-                {
-                    res += Cipher(input[i], (int)key[keyIndex]);
-                    keyIndex = keyIndex == key.Length - 1 ? 0 : keyIndex + 1;
-                }
-
-                return res;
-            }
-
-
-            public static string Decipher(string input, string key)
-            {
-                string res = "";
-                int keyIndex = 0;
-
-                for (int i = 0; i < input.Length; i++)
-                {
-                    res += Cipher(input[i], 52 - (int)key[keyIndex]);
-                    keyIndex = keyIndex == key.Length - 1 ? 0 : keyIndex + 1;
-                }
-
-                return res;
-            }
-
-            private static char Cipher(char ch, int key)
-            {
-                if (!char.IsLetter(ch)) return ch;
-                char offset = char.IsUpper(ch) ? 'A' : 'a';
-
-                return (char)((((ch + key) - offset) % 26) + offset);
-            }
         }
 
+    }
+
+    public class VignereCipher
+    {
+        public static string Encipher(string input, string key)
+        {
+            string res = "";
+            int keyIndex = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                res += Cipher(input[i], (int)key[keyIndex]);
+                keyIndex = keyIndex == key.Length - 1 ? 0 : keyIndex + 1;
+            }
+
+            return res;
+        }
+
+
+        public static string Decipher(string input, string key)
+        {
+            string res = "";
+            int keyIndex = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                res += Cipher(input[i], (int)key[keyIndex], true);
+                keyIndex = keyIndex == key.Length - 1 ? 0 : keyIndex + 1;
+            }
+
+            return res;
+        }
+
+        private static char Cipher(char ch, int key, bool reverse = false)
+        {
+            if (!char.IsLetter(ch)) return ch;
+
+
+            key = key % 26;
+            if (!reverse) return (char)(((ch + key - 97) % 26) + 97);
+            else return (char)(((ch + 26 - key - 97) % 26) + 97);
+
+        }
     }
 }
